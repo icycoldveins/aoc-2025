797811f0bb7a1f6c6f3c00759a06dddbc064d9ac
icycoldveins <kevinwijaya306@gmail.com>
2025-12-03 05:23:17 -0500

    Update

--- day1/day1.py
+++ day1/day1.py
@@ -34,6 +34,20 @@ def solve():
             if not pos:
                 cnt += 1
     return cnt
+'''
+now we somehow need to know when we make a rotation one way or not
+
+if we go left we pass 0 how can we capture passing 0 
+if we have a negative number or 0
+then what if we do that muiltiple times
+
+if we got right if we pass 0 
+
+if we get distance traveled going left or right after iterations 
+    if I know how much I travelled going so if I go left the distance has to be 
+    - 40 because we lost that much distance
+    + 40 if go right because we have extra distnce
+'''
 def solve2():
     pos = 50
     cnt = 0
@@ -41,13 +55,22 @@ def solve2():
         'L': -1,
         'R': 1
     }
+    def helper(pos, delta, right):
+        if not pos:
+            return delta // 100
+        if right:
+            return (pos + delta) // 100
+        if delta < pos:
+            return 0
+        return (delta - pos) // 100 + 1
+
     with open('day1/input.txt', 'r') as f:
         for line in f:
             line = line.strip()
             d = mem[line[:1]]
             m = int(line[1:])
-            pos += (d*m)
+            cnt += helper(pos, m, False if d < 0 else True)
+            pos += (d * m)
             pos %= 100
-            if not pos:
-                cnt += 1
     return cnt
+print(solve2())
